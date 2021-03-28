7cc64b3999967ce3e9cb6bc84c9a7b34a7f8c844
d28035203 <289484834+d28035203@users.noreply.github.com>
2021-03-28 21:44:10 +0530

    feat: random sample without replacement

---
+++ shuffle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""pyq-shuffle — random UPSC topic drill."""
+
+from __future__ import print_function
+import argparse, os, random, sys
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+TOPICS = os.path.join(HERE, "topics.txt")
+
+
+def load_topics():
+    lines = []
+    with open(TOPICS, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                lines.append(line)
+    return lines
+
+
+def main():
+    p = argparse.ArgumentParser(description="Shuffle revision topics")
+    p.add_argument("-n", type=int, default=3, help="how many topics (default 3)")
+    args = p.parse_args()
+    topics = load_topics()
+    if not topics:
+        print("topics.txt is empty")
+        return 1
+    n = max(1, min(args.n, len(topics)))
+    pick = random.sample(topics, n)
+    print("Your unfair revision draw:\n")
+    for i, t in enumerate(pick, 1):
+        print("  %d. %s" % (i, t))
+    print("\nNo skipping. The UPSC does not skip.")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
